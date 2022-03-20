c8ecbccea1d5f24d3d00df030233b7ae8ef8eb28
THELOSTSOUL <1095533751@qq.com>
2022-03-20 18:53:17 +0800

    Update sstatus.rs

--- src/register/sstatus.rs
+++ src/register/sstatus.rs
@@ -94,6 +94,21 @@ impl Sstatus {
     pub fn sd(&self) -> bool {
         self.bits.get_bit(size_of::<usize>() * 8 - 1)
     }
+    
+    #[inline]
+    pub fn set_spie(&mut self, val: bool) {
+        self.bits.set_bit(5, val);
+    }
+
+    #[inline]
+    pub fn set_sie(&mut self, val: bool) {
+        self.bits.set_bit(1, val);
+    }
+
+    #[inline]
+    pub fn set_spp(&mut self, val: SPP) {
+        self.bits.set_bit(8, val == SPP::Supervisor);
+    }
 }
 
 read_csr_as!(Sstatus, 0x100, __read_sstatus);
